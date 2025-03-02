a25a497ec597f242b7055d1a4dc257a550232ea4
vineet uprari <uprari@hotmail.com>
2025-03-02 17:23:26 +0530

    added lusitna

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,2 +1,3 @@
-import {Inter,Lora} from 'next/font/google';
+import {Inter,Lora,Lusitana} from 'next/font/google';
 export const inter= Inter({subsets:['latin']});
+export const lusitana= Lusitana({subsets:['latin'],weight:['400','700']});
