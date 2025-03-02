3caf55f7f40805145b60d01436d3a1c3ba2f6a21
vineet uprari <uprari@hotmail.com>
2025-03-02 17:43:13 +0530

    resolved latest invoice error

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -6,7 +6,7 @@ import { lusitana } from '@/app/ui/fonts';
 export default async function LatestInvoices({
   latestInvoices,
 }: {
-  latestInvoices: LatestInvoice[];
+  latestInvoices: [];
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
